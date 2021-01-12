fac7b1bc4ea619baecc160cefcaba021b5d117da
AhmedKhaled590 <62337087+AhmedKhaled590@users.noreply.github.com>
2021-01-12 17:21:39 +0200

    Final3

--- routes/Register.js
+++ routes/Register.js
@@ -27,15 +27,17 @@ router.post('/', function (req, res, next) {
     var age = req.body.age;
     var gov = req.body.Gov;
     var address = req.body.Address;
-    if(typeof num!='number') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid Phone number!" })
-    if(typeof num!='ssn') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid SSN!" })
+    //if(typeof num!='number') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid Phone number!" })
+//    if(typeof num!='ssn') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid SSN!" })
     if(confirmPassword!=password) return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Passwords do not match!" })
+    if(weight<0) return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Invalid Weight!" })
+    
     // lazem te3mel select 3al SSN l awal w lw rege3lek msh fady show message eno meta5ed
     db.all(`INSERT INTO DONOR(SSN,Fname,Minit,Lname,Age,Address,Phone_Num,Email,pass_word,Gender,Blood_Type,Logged,Notification)
           VALUES(?,?,?,?,?,?,?,?,?,?,?,1,'No Notifiction')`,
       [ssn, firstName, Minit, lastName, age, address, num, email, password, gender, bloodGroup],
       (err) => {
-        if (err) return console.log(err);
+        if (err) return res.render("error", { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg", message: "May You have already an email" })
         res.render('pages/Home', { title: "Blood Bank", css1: "home", css2: "Preq", css3: "animate", scrp: "home", UserName: firstName + " " + lastName })
       })
   }
@@ -55,9 +57,10 @@ router.post('/', function (req, res, next) {
     var gov = req.body.Gov;
     var address = req.body.Address;
     // const {SSN, FirstName} = req.body;
-    if(typeof num!='number') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid Phone number!" })
-    if(typeof num!='ssn') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid SSN!" })
+    //if(typeof num!='number') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid Phone number!" })
+    //if(typeof num!='ssn') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid SSN!" })
     if(confirmPassword!=password) return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Passwords do not match!" })
+
     db.all(`INSERT INTO PATIENT(SSN,Fname,Minit,Lname,Age,Address,Phone_Num,Email,pass_word,Gender,Blood_Type,Logged,Notification)
     VALUES(?,?,?,?,?,?,?,?,?,?,?,1,'No Notification')`,
     [ssn, firstName, Minit, lastName, age, address, num, email, password, gender, bloodGroup],
@@ -72,7 +75,7 @@ router.post('/', function (req, res, next) {
     var pass=req.body.pass;
     var number=req.body.PhoneNo
     var confirmPassword = req.body.confirmPassword;
-    if(typeof number!='number') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid Phone number!" })
+    //if(typeof number!='number') return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Enter a valid Phone number!" })
     if(confirmPassword!=pass) return res.render('pages/Register', { title: "Blood Bank", css1: "reg", css2: "", css3: "", scrp: "reg",msg:"Passwords do not match!" })
     db.all(`INSERT INTO ORGANIZATIONS(O_name,Email,Pass_word,Phone_Num,Location,logged,Notification)
             VALUES(?,?,?,?,?,1,'No  Notification')`,[OrganizationName,email,password,PhoneNo,Gov+''+Address],
